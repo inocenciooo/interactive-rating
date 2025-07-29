5588792ee2630379857d86c567e7c28eebc40198
vinicius inocêncio <134207832+inocenciooo@users.noreply.github.com>
2025-07-29 19:15:41 -0300

    Criação do botão voltar e da informação de qual nota foi selecionada.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 const card = document.getElementsByClassName("card")[0];
 const score = document.querySelectorAll(".score");
 const submit = document.getElementById("button");
+const backButton = document.getElementById("back");
+const scorePlace = document.getElementById("score-place");
 
 score.forEach((scoreNumber) => {
   scoreNumber.addEventListener("click", () => {
@@ -10,12 +12,21 @@ score.forEach((scoreNumber) => {
 
     scoreNumber.classList.add("select");
     card.classList.add("numberSelected");
+    scorePlace.textContent = scoreNumber.textContent;
   });
 });
 
-
 submit.addEventListener("click", enviarNota);
 
+backButton.addEventListener("click", () => {
+  card.classList.remove("numberSelected");
+  card.classList.remove("submit");
+
+  score.forEach((scoreNumber) => {
+    scoreNumber.classList.remove("select");
+  })
+
+})
 
 function enviarNota() {
   if (card.classList.contains("numberSelected")) {
