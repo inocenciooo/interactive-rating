777bd6d81061b6a4ff621cba29805b8468a21b82
vinicius inocêncio <vinicius1silva61@gmail.com>
2025-07-29 09:30:15 -0300

    feito seleção de notas e critério pra enviar

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,26 @@
 const card = document.getElementsByClassName("card")[0];
+const score = document.querySelectorAll(".score");
 const submit = document.getElementById("button");
 
-submit.addEventListener("click", enviarNota)
+score.forEach((scoreNumber) => {
+  scoreNumber.addEventListener("click", () => {
+    score.forEach((scoreNumber) => {
+      scoreNumber.classList.remove("select");
+    });
+
+    scoreNumber.classList.add("select");
+    card.classList.add("numberSelected");
+  });
+});
+
+
+submit.addEventListener("click", enviarNota);
 
 
 function enviarNota() {
+  if (card.classList.contains("numberSelected")) {
     card.classList.toggle("submit");
+  } else {
+    alert("Please select a rating!");
+  }
 }
